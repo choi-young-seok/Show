0602069a2e5c1c09eaa5776bdf3d840fae13d2c8
kosta <kosta@192.168.0.103>
2016-11-21 17:06:49 +0900

    관리자 jsp 및 css 등록

---
+++ Show/src/main/java/kr/co/show/admin/controller/AdminController.java
@@ -0,0 +1,23 @@
+package kr.co.show.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+	
+	@RequestMapping("/member")
+	public String admin_member(){
+		return "/admin/member";
+	}
+	
+	@RequestMapping("/admin_member_refly")
+	public String admin_member_refly(){
+		return"/admin/admin_member_refly";
+	}
+	
+	@RequestMapping("/shop")
+	public String admin_shop(){
+		return"/admin/shop";
+	}
+}
