40c64e596cd3e94b4b19fd117d942b936050ca15
KOSTA130 <KOSTA130@192.168.0.102>
2016-11-23 12:06:07 +0900

    adfsfa

--- Show/src/main/resources/mappers/db.sql
+++ Show/src/main/resources/mappers/db.sql
@@ -8,10 +8,10 @@ name varchar2(15) not null,
 nickname varchar2(15) not null,
 password varchar2(30) not null,
 phone varchar2(15) not null,
-birthday varchar2(10) not null,
+birthday date not null,
 allcheck VARCHAR2(10) check(allcheck in('T','F')),
-emailck VARCHAR2(10) check(emailck in('T','F')),
-smsck VARCHAR2(10) check(smsck in('T','F')),
+emailck VARCHAR2(10) check(emailck in('T','F')) default 'F',
+smsck VARCHAR2(10) check(smsck in('T','F')) default 'F',
 position VARCHAR2(10) check(position in('10','20','30'))
 );
 drop sequence member_seq;
@@ -21,5 +21,17 @@ increment by 1
 nocycle
 nocache;
 
-insert into member
-values (member_seq.nextval, 'aa@naver.com', '고길동', '둘리', 'asdf123','010-1234-1234', sysdate, 'T', 'T', 'T', '10');
+insert into show_member
+values (show_member_seq.nextval, 'aa@naver.com', '고길동', '둘리', 'asdf123','010-1234-1234', sysdate, 'T', 'T', 'T', '10');
+insert into show_member
+values (show_member_seq.nextval, 'bb@naver.com', '또치', '도너', 'asdf123','010-1234-1234', sysdate, 'T', 'T', 'T', '10');
+insert into show_member
+values (show_member_seq.nextval, 'cc@naver.com', '루피', '몽키', 'asdf123','010-1234-1234', sysdate, 'T', 'T', 'T', '10');
+
+
+insert into member (id, email, name, nickName, password, phone, birthday, allCheck, position)
+values (member_seq.nextval, '이메일', '이름', '별명', '비밀번호', '폰', '2011-12-23', 'T', 20);
+
+select *
+from show_member;
+
