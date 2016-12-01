d14e6f2744d2862a7fd040cfaeec760ae47f03e2
awayb <awayb@192.168.0.134>
2016-12-01 20:53:48 +0900

    asdf

--- Show/src/main/java/kr/co/show/admin/controller/AdminQnaController.java
+++ Show/src/main/java/kr/co/show/admin/controller/AdminQnaController.java
@@ -3,28 +3,30 @@ package kr.co.show.admin.controller;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.co.show.community.domain.Criteria;
+import kr.co.show.community.domain.PageMaker;
+import kr.co.show.community.service.QnaService;
+
 @Controller
-@RequestMapping("/admin")
+@RequestMapping
 public class AdminQnaController {
-	/*@Inject
-	private 서비스*/
+	@Inject
+	private QnaService service;
 	
-	@RequestMapping("/main2")
-	public String adminMain(){ //헤더, 푸터 들어있는 곳
+	@RequestMapping("/qna")
+	public String listQna(Model model,String qna_ch)throws Exception{
+		qna_ch="처리중";
+		model.addAttribute("lista",service.list(qna_ch));
 		
-		return "admin/adminMain2";
-	}
-	
-	@RequestMapping("/qnaList")
-	public String qnalist(){
 		return "admin/qna";
 	}
 	
-	@RequestMapping("/qnaview")
+	/*@RequestMapping("/qnaview")
 	public String selectFaq(){ //관리자 문의사항 글 조회
 		return "user/community/qna_board/view";
-	}
+	}*/
 		
 }
