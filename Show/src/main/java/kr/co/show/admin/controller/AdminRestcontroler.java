7141f7371eeb0130b78b56192f023fddd94827b2
kosta <kosta@192.168.0.130>
2016-12-08 16:25:48 +0900

    re

--- Show/src/main/java/kr/co/show/admin/controller/AdminRestcontroler.java
+++ Show/src/main/java/kr/co/show/admin/controller/AdminRestcontroler.java
@@ -6,7 +6,6 @@ import javax.inject.Inject;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
