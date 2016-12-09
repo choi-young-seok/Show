f5442859ad8000c843c19ed9f4f7f4c3ecf0db6f
Cys <rcn115@naver.com>
2016-12-09 16:36:45 +0900

    VO 파일명 배열로 변환

--- Show/src/main/java/kr/co/show/search/service/UploadServiceImpl.java
+++ Show/src/main/java/kr/co/show/search/service/UploadServiceImpl.java
@@ -2,16 +2,22 @@ package kr.co.show.search.service;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Service;
+
+import kr.co.show.search.persistence.UploadDAO;
 
 @Service
 public class UploadServiceImpl implements UploadService{
 	
+	@Inject
+	private UploadDAO dao;
 	
 	@Override
 	public List<String> getAttach(int group_no) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return dao.getAttach(group_no);
 	}
 
 }
