3e09152974bea24212447b97301f8193b9ce4ce5
KOSTA130 <KOSTA130@192.168.0.101>
2016-12-12 14:45:47 +0900

    v

--- Show/src/main/java/kr/co/show/sign/interceptor/AdminInterceptor.java
+++ Show/src/main/java/kr/co/show/sign/interceptor/AdminInterceptor.java
@@ -2,21 +2,27 @@ package kr.co.show.sign.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class SignInterceptor extends HandlerInterceptorAdapter{
+public class AdminInterceptor extends HandlerInterceptorAdapter{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		
-		return true;
+		HttpSession session = request.getSession();
+		if(session != null && session.getAttribute("admin_position").equals("30")){
+			System.out.println("세션값 30");
+			return true;
+		}
+		System.out.println("세션값 30이 아님");
+		return false;
 	}
-	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		
+		// TODO Auto-generated method stub
+		super.postHandle(request, response, handler, modelAndView);
 	}
 }
---
+++ Show/src/main/java/kr/co/show/sign/interceptor/SessionInterceptor.java
@@ -0,0 +1,27 @@
+package kr.co.show.sign.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SessionInterceptor extends HandlerInterceptorAdapter{
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		if(request.getSession().getAttribute("id")==null){
+			System.out.println("세션값 없음");
+			return false;
+		}
+		System.out.println("일반 회원 세션값 존재");
+		return true;
+	}
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+}
